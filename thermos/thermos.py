73f39100ffe21e5dd4a74d17ff5dc9b25c6d3fdc
Max S <m123@example.com>
2019-08-05 00:35:20 +0200

    final sunday

--- thermos/thermos.py
+++ thermos/thermos.py
@@ -1,7 +1,14 @@
-from flask import Flask,render_template,url_for
+from flask import Flask,render_template,url_for,request,redirect
 from User import User
-
+from datetime import  datetime
+from logging import DEBUG
 app=Flask(__name__)
+
+bookmark=[]
+
+def store_bookmark(url):
+    bookmark.append(dict(url=url,user="John",date = datetime.utcnow()))
+
 
 
 @app.route('/')
@@ -10,11 +17,24 @@ def index():
     user = User("jan", "kowalski")
     return render_template('index.html',Title="Title passed",user=user,Text=["asd","bcd","123"])
 
-@app.route('/add')
+@app.route('/add', methods=['GET','POST'])
 def add():
+    if request.method == "POST":
+        url = request.form['url']
+        store_bookmark(url)
+        app.logger.debug('stored url %s' % url)
+        return  redirect(url_for('index'))
     return render_template('add.html',Text="1232")
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'),404
+
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template('500.html'),500
 
 if __name__== "__main__":
     app.run(debug=True)
+    app.logger.setLevel(DEBUG)
 
